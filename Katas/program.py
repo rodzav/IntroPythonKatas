49cc2b14bd55a542046944e86777799b0fcd9eee
Rodrigo Zavala <rzr96019@gmail.com>
2022-02-13 01:21:27 -0600

    Nota: Ya subí las katas de la 1 a la 4

---
+++ Katas/program.py
@@ -0,0 +1,12 @@
+from datetime import date
+
+# program.py
+sum = 1 + 2
+print(sum)
+print('Hola desde la consola')
+
+print(type(sum))
+print(type('Hola desde la consola'))
+
+print('La fecha de hoy es ' + str(date.today()))
+
